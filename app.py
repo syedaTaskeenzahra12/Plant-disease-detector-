ab3b227478fa81b2983389f664bf7b4d33e1cabf
syedaTaskeenzahra12 <syedataskeen49@gmail.com>
2025-07-12 12:11:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,42 @@
 from flask import Flask, request, render_template
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import os
+
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    return "Plant Disease Detection Home"
+# Load the trained model
+model = load_model("model.h5")  # Make sure model.h5 is in the same folder
 
-if __name__ == "__main__":
+# Labels (example: change as per your dataset)
+class_labels = ['Healthy', 'Bacterial Spot', 'Early Blight', 'Late Blight']
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return "No file uploaded"
+
+    file = request.files['file']
+    if file.filename == '':
+        return "No image selected"
+
+    img_path = os.path.join("static", file.filename)
+    file.save(img_path)
+
+    img = image.load_img(img_path, target_size=(224, 224))
+    img_array = image.img_to_array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)
+    predicted_class = class_labels[np.argmax(prediction)]
+
+    return render_template('result.html', prediction=predicted_class, image_path=img_path)
+
+if __name__ == '__main__':
     app.run(debug=True)
